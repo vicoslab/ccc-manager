44888037f94bfead99aedcbeb24ac74e3257942c
Andraž Andolšek Peklaj <andraz.ap@pm.me>
2025-10-10 14:56:01 +0200

    Tolerate upstream commits by rebasing before push and reloading state after

--- commit.py
+++ commit.py
@@ -9,6 +9,16 @@ import config
 conv = Ansi2HTMLConverter()
 convert = lambda x: f'<div>{conv.convert(x, full=False)}</div>'
 
+base_diff = subprocess.run(
+    ['git', 'diff', '--color', '--ws-error-highlight=all'],
+    cwd='/opt/ccc-inventory',
+    capture_output=True
+).stdout.decode().splitlines()
+
+if base_diff:
+    print('Warning: found unmanaged changes, deleting')
+    subprocess.run(['git', 'restore', '.'], cwd='/opt/ccc-inventory')
+
 st.session_state.diff_size = 0
 with tempfile.NamedTemporaryFile('w') as f:
     save_users(st.session_state, f)
@@ -47,18 +57,30 @@ def discard():
     with open(config.containers) as f:
         load_containers(st.session_state, f)
 
-@st.dialog('Commit changes')
+@st.dialog('Commit changes', width='large')
 def commit():
     with st.form('commit-form'):
         message = st.text_input('Short description of the changes')
-        if st.form_submit_button('Commit'):
+        if st.form_submit_button('Commit') and message:
             with open(config.users, 'w') as f:
                 save_users(st.session_state, f)
                 
             with open(config.containers, 'w') as f:
                 save_containers(st.session_state, f)
-            subprocess.run(['bash', 'commit.sh', message])
-            st.rerun()
+            p = subprocess.run(['bash', 'commit.sh', message], capture_output=True)
+            # Don't discard if script failed as that would import uncommitted changes and no longer show them
+            # The changes which we saved will get reverted from the file upon rerun
+            if p.returncode == 0:
+                discard()
+                st.rerun()
+            else:
+                out = p.stdout.decode().splitlines()
+                st.write(':red[Could not pull with rebase. Remove any changes causing a merge conflict.]')
+                st.divider()
+                st.html(f'''
+                    {conv.produce_headers()}
+                    <pre class='ansi2html-content'>{''.join(map(convert, out))}</pre>
+                ''')
 
 if st.session_state.diff_size > 0:
     with st.container(horizontal=True):
--- commit.sh
+++ commit.sh
@@ -8,4 +8,16 @@ fi
 cd /opt/ccc-inventory/inventory/group_vars/ccc-cluster/
 
 git add user-list.yml user-containers.yml
-git commit -m "$1"
+git commit -m "$1" &> /dev/null
+
+git pull --rebase &> /dev/null
+STATUS=$?
+if [[ $STATUS -ne 0 ]]; then
+    echo 'Rebase not successful' >&2
+    git diff --diff-filter=U --color
+    git rebase --abort
+    git reset --hard HEAD~1 &> /dev/null
+    exit 1
+fi
+
+git push
