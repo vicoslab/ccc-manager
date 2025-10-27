11161fb795b77e115829fdc5b230b89d1856ae59
Andraž Andolšek Peklaj <andraz.ap@pm.me>
2025-10-27 10:22:54 +0100

    Get all necessary info when creating user and switch directly to apply page

--- manage-users.py
+++ manage-users.py
@@ -5,24 +5,34 @@ import unicodedata
 container_df = st.session_state['container_df']
 user_df = st.session_state['user_df']
 
-@st.dialog('Add user', on_dismiss='rerun')
+@st.dialog('Add user', on_dismiss='rerun', width='medium')
 def add_user(k, df):
-    with st.form(key='add-user'):
         fullname = st.text_input('Full name', key=f'user-{k}-fullname')
         email = st.text_input('Email', key=f'user-{k}-email')
-        if st.form_submit_button('Add') and fullname and email:
+        mentor = st.selectbox('Mentor', st.session_state.mentors, st.session_state['mentor_view'], accept_new_options = True)
+        
+        cols = st.columns([2,6])
+        pubkey = cols[0].segmented_control('Use public key from', options = ['Text', 'GitHub'], default='Text')
+        
+        if pubkey == 'Text':
+            key = cols[1].text_input('Public key', '', key=f'user-{k}-pk')
+        else:
+            key = cols[1].text_input('Github username', '', key=f'user-{k}-pkgh')
+
+        if st.button('Add') and fullname and email and key:
             type_ = {'Researcher': 'researcher', 'PhD': 'researcher', 'Student': 'student', 'LKM': 'student_LKM'}[k]
             name = unicodedata.normalize('NFKD', fullname).encode('ascii','ignore').decode().lower().split(' ')
             df.loc[email, ['USER_FULLNAME', 'USER_TYPE', 'USER_EMAIL', 'USER_NAME']] = \
                 [fullname, type_, email, name[0]]
             
-            if st.session_state['mentor_view']:
-                df.loc[email, 'USER_MENTOR'] = st.session_state['mentor_view']
+            df.loc[email, 'USER_PUBKEY' if pubkey == 'Text' else 'USER_PUBKEY_FROM_GITHUB'] = key
+
+            if mentor:
+                df.loc[email, 'USER_MENTOR'] = mentor
             
             add_container_with_defaults(container_df[k], name, f'{"".join(name)}-workspace', email)
 
-            st.session_state['selected_user'] = k, email
-            st.switch_page('edit-user.py')
+            st.switch_page('commit.py')
 
 if st.session_state['mentor_view']:
     st.html('''
